cc9f5c1882e8b864c9bcb164363a9fd60c55277d
Camila Maira <camilamaira.m@gmail.com>
2022-07-31 00:36:30 -0400

    fetch working

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const key = "lista_tareas";
+const name_key = "name_input"
+const tareasJSON = "./tareas.json";
 
-const tareas = [
+/*const tareas = [
   { id: 1, contenido: "Entrega CoderHouse", estado: "Para hoy", tag: "Personal" },
   { id: 2, contenido: "Aseo Semanal", estado: "Hecho", tag: "Casa" },
   { id: 3, contenido: "Junta con Nico", estado: "Por hacer", tag: "Personal" },
   { id: 4, contenido: "Curso Udemy", estado: "Para hoy", tag: "Trabajo" },
   { id: 5, contenido: "Almuerzo con mamá", estado: "Hecho", tag: "Casa" },
   { id: 6, contenido: "Estudio AM", estado: "Por Hacer", tag: "Personal" },
-  { id: 6, contenido: "Jueves 15.00: Reunión de Contenidos", estado: "Hecho", tag: "Trabajo" },
-];
+  { id: 7, contenido: "Jueves 15.00: Reunión de Contenidos", estado: "Hecho", tag: "Trabajo" },
+];*/
 
 class Tarea {
   constructor(tarea) {
@@ -31,20 +33,22 @@ let btnSend = document.querySelector("#btnSend");
 let mainContenedor = document.querySelector("#contenedorTareas");
 let contenedorNombre = document.querySelector("#nombre");
 let select = document.querySelector("#select");
+let tag = document.querySelector("#selectTag")
 
 //PREGUNTAR NOMBRE
 
 askNombre = () => {
     Swal.fire({
-      title: "Bienvenide a la mejor TO-DO List del planeta",
+      title: "Bienvenide a TO-DO List",
       text: "Escribe tu nombre",
       input: 'text',
       showCancelButton: true        
   }).then((result) => {
       if (result.value) {
-          const input = document.createElement("p");
-          input.innerHTML = `Hola, ${result.value}`;
-          contenedorNombre.appendChild(input);
+          const inputName = document.createElement("p");
+          inputName.innerHTML = `Hola, ${result.value}`;
+          inputName.classList.add("name-input")
+          contenedorNombre.appendChild(inputName);
       }
     })
 };
@@ -58,16 +62,17 @@ btnSend.onclick = () => {
 // PUSH NUEVA TAREA
 sendTarea = () => {
   //ID
-  let largoArray = tareas.length;
+  let largoArray = newTareas.length;
   const id = largoArray + 1;
-  const selectValue = select.options[select.selectedIndex].value; 
+  const selectEstado = select.options[select.selectedIndex].value;
+  const selectTag = tag.options[tag.selectedIndex].value;
+
 
   const tarea = input.value;
   if (!tarea) {
     return } 
-    tareas.push({id: id, contenido: tarea, tag: selectValue, estado: false });
-    console.log(tareas);
-
+    newTareas.push({id: id, contenido: tarea, tag: selectTag, estado: selectEstado });
+  
   guardarStorage();
   refreshLista();
 };
@@ -75,34 +80,78 @@ sendTarea = () => {
 
 //LOCAL STORAGE
 	const obtenerTareas = () => {
-		const itemList = JSON.parse(localStorage.getItem(key));
-		if (itemList) {
-			return itemList;
-		} else {
-			return [];
-		}
-	};
+	const itemList = JSON.parse(localStorage.getItem(key));
+	if (itemList) {
+    return itemList;
+	} else {
+	return [];
+}};
 
-	const guardarStorage = () => {
-		localStorage.setItem(key, JSON.stringify(tareas));
-    //localStorage.setItem(key, JSON.stringify(input));
-	};
+const guardarStorage = () => {
+	localStorage.setItem(key, JSON.stringify(newTareas));
+};
+
+//// FETCH
+const getTareas = () => {
+  mainContenedor.innerHTML='Cargando..';
+  fetch("./tareas.json")
+  .then((res) => {
+    return res.json()
+  })
+  .then(tareas => {
+    for (const tarea of Object.values(tareas)) {
+          const span = document.createElement("span");
+          const p = document.createElement("p");
+          const p2 = document.createElement("p");
+          span.textContent = tarea.contenido;
+          p.textContent = tarea.tag;
+          p2.textContent = tarea.estado;
+          const itemDiv = document.createElement("div");
+          itemDiv.classList.add("tarea-contenedor");
+          
+          if (tareas.contenido) {
+            itemDiv.appendChild(span);
+            itemDiv.appendChild(p)
+            itemDiv.appendChild(p2)
+            //itemDiv.appendChild(deleteItem);
+            mainContenedor.appendChild(itemDiv);
+        }
+    }
+  })
+};
+
+getTareas();
+
+  // INSERTAR TAREAS EN EL DOM
+const cargarTareas = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const res = true;
+      if (res) {
+        resolve(newTareas);
+      } else {
+        reject(400);
+      }
+    }, 2000)
+  })
+}; 
+
 
 	// Definir función que refresca la lista de tareas a partir del arreglo global
 const refreshLista= () => {
 	mainContenedor.innerHTML = "";
-	  for (const [indice, tarea] of tareas.entries()) {
+	for (const [indice, tarea] of newTareas.entries()) {
 
-			// ENLACE PARA ELIMINAR DEL DOM TAREAS
-     const deleteItem = document.createElement("a");
+			// ENLACE PARA ELIMINAR DEL DOM ALGUNA TAREAS
+    const deleteItem = document.createElement("a");
 			deleteItem.classList.add("enlace-eliminar");
 			deleteItem.innerHTML = "&times;";
 			deleteItem.href = "";
 			deleteItem.onclick = (e) => {
 				e.preventDefault();
 				if (!Swal.fire({
-          title: 'Estas segure?',
-          text: "muy muy segure?",
+          title: '¿Deseas eliminar esta tarea?',
+          text: "¿Segur@?",
           icon: 'warning',
           showCancelButton: true,
           confirmButtonColor: '#3085d6',
@@ -111,34 +160,39 @@ const refreshLista= () => {
         }).then((result) => {
           if (result.isConfirmed) {
             Swal.fire(
-              'Borrado',
-              'Tu tarea ha sido borrada de nuestra base de datos',
+              '¡Listo!',
+              'Tu tarea ha sido eliminada de nuestra base de datos',
               'success'
             )
           }
         })) {
 					return;
 				}
-				tareas.splice(indice, 1);
+				newTareas.splice(indice, 1);
 				// GUARDAR CAMBIOS
 				guardarStorage();
 				refreshLista();
 			};
 
 			//SPAN CON TAREA.CONTENIDO
-			const span = document.createElement("span");
-      const p = document.createElement("p");
-			span.textContent = tarea.contenido;
-      p.textContent = tarea.tag;
-			const itemDiv = document.createElement("div");
-      itemDiv.classList.add("tarea-contenedor");
-      
-			if (tarea.contenido) {
-				itemDiv.appendChild(span);
-        itemDiv.appendChild(p)
-			  itemDiv.appendChild(deleteItem);
-			  mainContenedor.appendChild(itemDiv);
-			}
+     
+      cargarTareas();
+         const span = document.createElement("span");
+         const p = document.createElement("p");
+         const p2 = document.createElement("p");
+         span.textContent = tarea.contenido;
+         p.textContent = tarea.tag;
+         p2.textContent = tarea.estado;
+         const itemDiv = document.createElement("div");
+         itemDiv.classList.add("tarea-contenedor");
+          
+         if (tarea.contenido) {
+           itemDiv.appendChild(span);
+           itemDiv.appendChild(p)
+           itemDiv.appendChild(p2)
+           itemDiv.appendChild(deleteItem);
+           mainContenedor.appendChild(itemDiv);
+      }
 		}
 	};
 
